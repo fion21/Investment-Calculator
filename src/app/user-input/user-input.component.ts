a246f65609351b678e5963e4548c2abb05e70d9c
Fiona L <hwindepth@yahoo.com>
2025-01-03 22:48:10 +0000

    4th c cross communication output

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -9,17 +9,24 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  @Output() calculate = new EventEmitter<{
+  initialInvestment: number,
+  duration: number,
+  expectedReturn: number,
+  annualInvestment: number
+}>();
   enteredInitialInvestment = '0';
   enteredAnnualInvestment = '0';
   enteredExpectedReturn = '5';
   enteredDuration = '0';
 
   onSubmit() {
-    console.log('SUBMITTED');
-    console.log('Initial Investment: ' + this.enteredInitialInvestment);
-    console.log('Annual Investment: ' + this.enteredAnnualInvestment);
-    console.log('Expected Return: ' + this.enteredExpectedReturn);
-    console.log('Duration: ' + this.enteredDuration);
-  }
+    this.calculate.emit({
+      initialInvestment: + this.enteredInitialInvestment,
+      duration: + this.enteredDuration,
+      expectedReturn: + this.enteredExpectedReturn,
+      annualInvestment:  + this.enteredAnnualInvestment
+    });
 
+  }
 }
--- src/investment-results.ts
+++ src/investment-results.ts
@@ -2,7 +2,7 @@
 // e.g., integrate it into a service or component
 // You may need to tweak it, depending on where and how you use it
 
-function calculateInvestmentResults() {
+function calculateInvestmentResults(): any {
   const annualData = [];
   let investmentValue = initialInvestment;
 
